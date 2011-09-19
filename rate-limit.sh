4454d2d080cc7a225ac3e1172d35388ef510b99d
Jie Li <jacklee1985@gmail.com>
2011-09-19 03:17:50 -0400

    mvctp_v2 updated.

---
+++ rate-limit.sh
@@ -0,0 +1,17 @@
+DEV=$1
+DPORT=$2
+RATE=$3
+
+#tc qdisc del dev $DEV root
+#tc qdisc add dev $DEV handle 1: root htb
+#tc class add dev $DEV parent 1: classid 1:1 htb rate 50Kbps
+#tc filter add dev $DEV parent 1: protocol ip prio 16 u32 match ip dport $DPORT 0xffff flowid 1:1
+
+#tc qdisc del dev eth0 root
+
+
+tc qdisc del dev $DEV root
+tc qdisc add dev $DEV handle 1: root htb 
+tc class add dev $DEV parent 1: classid 1:1 htb rate $RATE #50Kbps
+tc qdisc add dev $DEV parent 1:1 handle 10:1 netem loss 1%
+tc filter add dev $DEV parent 1: protocol ip prio 16 u32 match ip dport $DPORT 0xffff flowid 1:1
