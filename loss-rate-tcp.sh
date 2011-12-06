811fe2ae52249ee6d4477fdb1216e81b0777cab6
Jie Li <jacklee1985@gmail.com>
2011-12-06 02:43:40 -0500

    mvctp_v2 updated.

--- loss-rate-tcp.sh
+++ loss-rate-tcp.sh
@@ -11,6 +11,6 @@ LOSS_RATE=$3
 
 tc qdisc del dev $DEV root
 tc qdisc add dev $DEV handle 1: root htb
-tc class add dev $DEV parent 1: classid 1:1 htp rate 1000Mbps
+tc class add dev $DEV parent 1: classid 1:1 htb rate 1000Mbps
 tc qdisc add dev $DEV parent 1:1 handle 10:1 netem loss $LOSS_RATE
 tc filter add dev $DEV parent 1: protocol ip prio 1 u32 match ip sport $SPORT 0xffff flowid 1:1
