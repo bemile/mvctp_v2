62e04e66331a1825f294d3950f64a268b8257269
Jie Li <jacklee1985@gmail.com>
2012-01-01 21:28:59 -0500

    mvctp_v2 updated.

--- MVCTP/TcpServer.cpp
+++ MVCTP/TcpServer.cpp
@@ -77,12 +77,12 @@ void TcpServer::SendToAll(const void* data, size_t length) {
 		}
 	}
 
-	for (it = bad_sock_list.begin(); it != bad_sock_list.end(); it++) {
-		close(*it);
-		conn_sock_list.remove(*it);
-		FD_CLR(*it, &master_read_fds);
-		cout << "TcpServer::SendToAll()::One socket deleted: " << *it << endl;
-	}
+//	for (it = bad_sock_list.begin(); it != bad_sock_list.end(); it++) {
+//		close(*it);
+//		conn_sock_list.remove(*it);
+//		FD_CLR(*it, &master_read_fds);
+//		cout << "TcpServer::SendToAll()::One socket deleted: " << *it << endl;
+//	}
 	pthread_mutex_unlock(&sock_list_mutex);
 }
 
