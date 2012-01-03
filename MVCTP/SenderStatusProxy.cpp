37e28895f06b336b1fbde9cfbd799093afa8206d
Jie Li <jacklee1985@gmail.com>
2012-01-03 00:55:22 -0500

    mvctp_v2 updated.

--- MVCTP/SenderStatusProxy.cpp
+++ MVCTP/SenderStatusProxy.cpp
@@ -55,8 +55,9 @@ void SenderStatusProxy::InitializeExecutionProcess() {
 int SenderStatusProxy::SendMessageToManager(int msg_type, string msg) {
 	if (msg_type == EXP_RESULT_REPORT) {
 		cout << "I received exp report: " << msg << endl;
+		result_file << msg;
 		if (result_file.is_open()) {
-			result_file << msg;
+
 			//result_file << exp_manager.GetFileSize() << "," << exp_manager.GetSendRate() << "," << msg;
 		}
 		//exp_manager.HandleExpResults(msg);
