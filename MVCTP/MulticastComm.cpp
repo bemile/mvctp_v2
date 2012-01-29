d1936341d83c2401a0bbec71771bb26ba53bc592
Jie Li <jacklee1985@gmail.com>
2012-01-29 16:42:11 -0500

    mvctp_v2 updated.

--- MVCTP/MulticastComm.cpp
+++ MVCTP/MulticastComm.cpp
@@ -38,7 +38,7 @@ int MulticastComm::JoinGroup(const SA* sa, int sa_len, const char *if_name) {
 				memcpy(&mreq.imr_interface, &((struct sockaddr_in *) &if_req.ifr_addr)->sin_addr,
 						sizeof(struct in_addr));
 
-				//setsockopt(sock_fd, SOL_SOCKET, SO_BINDTODEVICE, if_name, strlen(if_name));
+				setsockopt(sock_fd, SOL_SOCKET, SO_BINDTODEVICE, if_name, strlen(if_name));
 			}
 			else {
 				mreq.imr_interface.s_addr = htonl(INADDR_ANY);
