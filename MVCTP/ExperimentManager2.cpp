02ade6caea6bd127cdf6b19192e2ee162909c90d
Jie Li <jacklee1985@gmail.com>
2012-09-03 12:33:42 -0400

    vcmtp updated.

--- MVCTP/ExperimentManager2.cpp
+++ MVCTP/ExperimentManager2.cpp
@@ -47,7 +47,6 @@ void ExperimentManager2::GenerateFiles() {
 	char buf[BUF_SIZE];
 	int count = 0;
 	while (infile >> size) {
-		cout << "Generating file of size " << size << endl;
 		int remained_size = size;
 		sprintf(file_name, "/tmp/temp/temp%d.dat", file_index++);
 		ofstream outfile (file_name, ofstream::binary | ofstream::trunc);
