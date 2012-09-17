9cc8c88ef84c63a7f9f66c75a6372b5aea27bfb7
Jie Li <jacklee1985@gmail.com>
2012-09-17 10:31:29 -0400

    vcmtp updated.

--- MVCTP/ExperimentManager2.cpp
+++ MVCTP/ExperimentManager2.cpp
@@ -247,7 +247,7 @@ void ExperimentManager2::StartExperiment2(SenderStatusProxy* sender_proxy, MVCTP
 			}
 
 			sprintf(file_name, "/tmp/temp/temp%d.dat", i + 1);
-			file_id = sender->SendFile(file_name);
+			file_id = sender->SendFile(file_name, 10000);
 		}
 
 		while (!sender->IsTransferFinished(file_id)) {
